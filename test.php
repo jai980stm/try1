bdb2e3333f43bba77879018443b012508b02739e
jai980stm <84760307+jai980stm@users.noreply.github.com>
2021-05-25 12:33:03 +0800

    try1
    mytry1

---
+++ test.php
@@ -0,0 +1,8 @@
+<body>
+<form action="welcome.php" method="post">
+Nama Planet:<input type="text" name="planet"><br>
+Jejari(km) :<input type="text" name="jejari"><br>
+<input type="submit">
+</form>
+</body>
+</html>
